f0499aee90454630083c7902fe1933819ebd9e4a
OsamaAkilaDev <osama.akila20@gmail.com>
2025-11-19 22:33:46 +0400

    switch to bcryptjs

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,4 +1,4 @@
-import { compare, hash } from "bcrypt";
+import { compare, hash } from "bcryptjs";
 import { Request, Response } from "express";
 import { prisma } from "..";
 import * as jwt from "jsonwebtoken";
